90875c53e5e02160aaa7676e6f281ec34cddae1c
Saranya Vignesh <saranyachandran204@gmail.com>
2017-06-22 18:39:58 +0530

    New files added

---
+++ iPhone Apps/testViewController.h
@@ -0,0 +1,13 @@
+//
+//  testViewController.h
+//  iPhone Apps
+//
+//  Created by Saranya Vignesh on 22/06/17.
+//  Copyright © 2017 Naga Mac Mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testViewController : UIViewController
+
+@end
---
+++ iPhone Apps/testViewController.m
@@ -0,0 +1,37 @@
+//
+//  testViewController.m
+//  iPhone Apps
+//
+//  Created by Saranya Vignesh on 22/06/17.
+//  Copyright © 2017 Naga Mac Mini. All rights reserved.
+//
+
+#import "testViewController.h"
+
+@interface testViewController ()
+
+@end
+
+@implementation testViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
